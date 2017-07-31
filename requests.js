3552c09eadfee253b33d518f09e351382a402eef
Emanuel Neves <emanuelneves10@gmail.com>
2017-07-31 16:03:20 +0000

    completed

---
+++ requests.js
@@ -0,0 +1,14 @@
+var request = require('request');
+var fs = require('fs');
+
+console.log("Downloading image...");
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', function(err){
+    throw err;
+  })
+  .on('response', function(response){
+    console.log("Download complete!")
+    console.log(response.statusMessage, response.headers['content-type']);
+  })
+  .pipe(fs.createWriteStream('./future.jpg'));
